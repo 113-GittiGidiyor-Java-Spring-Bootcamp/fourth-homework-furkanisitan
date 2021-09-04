ea02ce3809cf79ccbe469c8085e7d1b1e4dc74ac
Furkan Işıtan <furkanisitan96@gmail.com>
2021-09-05 01:11:14 +0300

    Create ResponseMessages.java

---
+++ src/main/java/dev/patika/schoolmanagementsystem/core/constants/ResponseMessages.java
@@ -0,0 +1,9 @@
+package dev.patika.schoolmanagementsystem.core.constants;
+
+public class ResponseMessages {
+
+    public static final String NOT_FOUND = "No records found.";
+    public static final String UNIQUE_CONSTRAINT = "A unique constraint error has occurred.";
+    public static final String FOREIGN_KEY_CONSTRAINT = "A foreign key constraint error has occurred.";
+    public static final String INVALID_ENTITY_TYPE = "An invalid entity type error has occurred.";
+}
