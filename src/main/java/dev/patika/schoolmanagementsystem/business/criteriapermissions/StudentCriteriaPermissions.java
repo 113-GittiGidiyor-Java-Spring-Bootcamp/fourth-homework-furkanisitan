bfeef79eb80c21b1dc335284a7ead509a9578404
Furkan Işıtan <furkanisitan96@gmail.com>
2021-09-07 11:41:34 +0300

    Add business.criteriapermissions

---
+++ src/main/java/dev/patika/schoolmanagementsystem/business/criteriapermissions/StudentCriteriaPermissions.java
@@ -0,0 +1,7 @@
+package dev.patika.schoolmanagementsystem.business.criteriapermissions;
+
+import dev.patika.schoolmanagementsystem.core.criteria.CriteriaPermissions;
+import dev.patika.schoolmanagementsystem.entities.Student;
+
+public interface StudentCriteriaPermissions extends CriteriaPermissions<Student> {
+}
